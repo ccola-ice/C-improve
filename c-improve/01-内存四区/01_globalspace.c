7e949e69b76faf10594edfa7366a11200b73517f
ccola-ice <zb18747639545@163.com>
2023-11-26 17:19:50 +0800

    //trim

--- c-improve/01-内存四区/01_globalspace.c
+++ c-improve/01-内存四区/01_globalspace.c
@@ -11,7 +11,7 @@ void func()
     s_a++;
     printf("s_a = %d\n", s_a);
 }
-void test03()
+void test01()
 {
     func();     //static int s_a = 10会执行
     func();     //static int s_a = 10不会执行
@@ -19,7 +19,7 @@ void test03()
 }
 
 //未初始化的静态变量会被初始化为0
-void test04()
+void test02()
 {
     static int s_b;
     printf("s_b = %d\n",s_b);
@@ -28,7 +28,7 @@ void test04()
 //const修饰的变量
 //const修饰的全局变量：全局变量保存在常量区/静态区/全局区，受到常量区的保护，上面的值不可以被修改
 const int a = 10;
-void test05()
+void test03()
 {
     //a = 20;   //直接修改，失败
     //int * p = &a;
@@ -36,7 +36,7 @@ void test05()
     //printf("a = %d\n",a);   //间接修改，失败
 }
 //const修饰的局部变量：
-void test06()
+void test04()
 {
     const int b = 10;   //虽然仍是const修饰，
     //b = 20;     //直接修改，失败 
@@ -45,8 +45,32 @@ void test06()
     printf("b = %d\n",b);   //间接修改，成功
 }
 
+//字符串常量：
+//1、字符串常量是共享的
+void test05()
+{
+    char * p1 = "hello world";
+    char * p2 = "hello world";
+    char * p3 = "hello world";
+    printf("%p\n",&("hello world"));
+    printf("%p\n",p1);
+    printf("%p\n",p2);
+    printf("%p\n",p3);  //三个不同的指针的地址值是相同的，指向同一个字符串常量。
+}
+//2、在mingw环境下，不允许修改字符串常量对应的内存，否则程序会崩溃
+void test06()
+{
+    char * p = "hello world";
+    printf("%c\n",p[0]);
+    //p[0] = 'x';
+    printf("%s\n",p);
+}
+
+
 int main()
 {
+    test01();
+    test02();
     test03();
     test04();
     test05();
