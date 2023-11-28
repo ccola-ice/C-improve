9a04262eee47984bb762b58a6cbfb43ec1c96b7a
ccola-ice <zb18747639545@163.com>
2023-11-28 09:52:30 +0800

    //指针进阶

--- c-improve/02-指针进阶/02-pointer1.c
+++ c-improve/02-指针进阶/02-pointer1.c
@@ -5,12 +5,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+//野指针指向一个已释放的内存或者未申请过的内存空间,程序报错
 void test01()
 {
+    char * p = NULL;
+    //给p指向的内存区域拷贝内容
+    strcpy(p, "1111"); //err
 
+    char * q = 0x1122;
+    //给q指向的内存区域拷贝内容
+    strcpy(q, "2222"); //err
 }
 
+
+int* func()
+{
+    int a = 10;
+    int * p = &a;
+    return p;
+}
+//常见的几种野指针
+void test02()
+{
+    //1 指针变量未初始化
+    int * p1;
+    //printf("%d\n", *p1);
+
+    //2 指针释放后未置空
+    int * p2 = (int*)malloc(sizeof(int));
+    *p2 = 100;
+    free(p2);
+    printf("%d\n", *p2); //乱码 已经释放了
+
+    //3 指针操作超越变量作用域，其实就是栈返回局部变量的地址
+    int * p3 = func();
+    printf("%d\n", *p3);
+}
 int main()
 {
-    test01();
+    //test01();
+    test02();
 }
